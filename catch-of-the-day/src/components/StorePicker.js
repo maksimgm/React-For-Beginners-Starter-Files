fbc78ea71bb37dbe3f54269aa74a3c0e8e50e519
maksimgm <maksimmamrikov@gmail.com>
2017-02-19 21:48:53 -0800

    add router and method to StorePicker class

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component{
-    render(){
-      return (
-          <form className="store-selector">
+  goToStore(event){
+    event.preventDefault();
+    this.storeInput.value
 
-            <h2>Please Enter a Store</h2>
-            <input type="text" required placeholder="Store Name" />
-            <button type="submit"> Visit Store </button>
-          </form>
-      )
-    }
+  }
+
+  render(){
+    return (
+        <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
+          <h2>Please Enter a Store</h2>
+          <input type="text" required placeholder="Store Name"
+          defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
+          <button type="submit"> Visit Store </button>
+        </form>
+    )
+  }
 }
 
 export default StorePicker;
