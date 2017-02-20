c139d8d28eeef4e239a08e86e26c500eedea9f54
maksimgm <maksimmamrikov@gmail.com>
2017-02-20 15:12:58 -0800

    integrate firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDdibVdWPSMmoGkhijkY7mY8UPspN1NHdg",
+  authDomain: "catch-of-the-day-8e155.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-8e155.firebaseio.com",
+});
+
+export default base;
